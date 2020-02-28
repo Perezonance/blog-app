c16c56241e1cc4cfd94721d975c827af16dbcf42
Manuel Perez <manuel.parez@toyota.com>
2020-02-27 23:04:09 -0600

    init commit

---
+++ backend/AuthService/service.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/alexperez/blog-app/proto"
+	"google.golang.org/grpc"
+)
+
+type authServer struct {
+}
+
+func (authServer) Login(_ context.Context, in *proto.LoginRequest) (*proto.AuthResponse, error) {
+	return &proto.AuthResponse{}, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+
+	proto.RegisterAuthServiceServer(server, authServer{})
+
+	listener, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal("Error creating listener: ", err.Error())
+	}
+
+	server.Serve(listener)
+}
